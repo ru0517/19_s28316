d0fa61f0652a7d6acc7896bdba97e1f44aba171b
ru0517 <teimurovr@gmail.com>
2024-03-15 18:53:39 +0100

    com

---
+++ Tasks.py
@@ -0,0 +1,4 @@
+# Using list comprehension to generate a list of squares of numbers from 1 to 10
+squares = [x**2 for x in range(1, 11)]
+print("List of squares of numbers from 1 to 10:")
+print(squares)
