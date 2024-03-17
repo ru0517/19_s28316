306db688ec256ad3f199d953fd8015896bab7a93
ru0517 <teimurovr@gmail.com>
2024-03-17 21:04:57 +0100

    I made all tasks

---
+++ Task6/task6.py
@@ -0,0 +1,10 @@
+from square_generator import SquareGenerator
+
+square_gen = SquareGenerator()
+start = 1
+end = 10
+try:
+    squares = square_gen.generate_squares(start, end)
+    print(squares)
+except ValueError as e:
+    print(e)
